f588ad47a2277bbe6c3419c40976926e7eb14dee
s1240155 <38268545+s1240155@users.noreply.github.com>
2018-07-08 20:49:07 +0900

    Create ex03.c

---
+++ ex03.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+typedef struct ff {
+
+ int fv1;
+ int fv2;
+
+} fv;
+
+
+
+
+
+
+int getFaceValue(){
+
+ int res;
+
+ res = rand() % 6 + 1;
+
+ return res;
+}
+
+
+
+fv DiceGame(){
+
+ fv f;
+
+ f.fv1 = getFaceValue();
+ f.fv2 = getFaceValue();
+
+ return f;
+
+}
+
+
+int main(){
+
+ int sum;
+
+ fv v;
+
+ printf("Rolling the dice\n");
+
+ v = DiceGame();
+
+ printf("Die 1: %d\n",v.fv1);
+ printf("Die 2: %d\n",v.fv2);
+
+
+ sum = v.fv1 + v.fv2;
+
+ printf("Total value: %d\n",sum);
+
+ return 0;
+
+}
